d9c603f453b34378dfeedd9311b15b37eeec6e43
yui0303 <iampeter62@gmail.com>
2023-12-22 15:19:59 +0800

    combine camera2

--- examples/detector.c
+++ examples/detector.c
@@ -1,5 +1,6 @@
 #include "darknet.h"
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
@@ -580,6 +581,7 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
     char *input = input_pic_path;
     float nms=.45;
     // print current directory
+    printf("Current directory: ");
     system("pwd");
     while(1){
         // printf("In while loop\n");
